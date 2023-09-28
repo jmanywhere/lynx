f7614138802fd99775f0df64534087b75555f332
Semi Invader <=>
2023-09-28 09:26:15 -0600

    lynx token deployed

--- src/LynxToken.sol
+++ src/LynxToken.sol
@@ -8,12 +8,15 @@ import "./interfaces/IUniswap.sol";
 error LYNX__MaxWalletReached(address wallet, uint triedBalance);
 error LYNX__Blacklisted();
 error LYNX__InvalidThreshold();
+error LYNX__TradingNotEnabled();
+error LYNX__NotAllowed();
+error LYNX__InvalidTaxAmount();
+error LYNX__InvalidMaxWallet();
 
 contract Lynx is Ownable, ERC20 {
     //---------------------------------------------------------------------------------
     // Structs
     //---------------------------------------------------------------------------------
-
     struct SnapshotInfo {
         uint tier1Total; // Tier 1 eligible balance
         uint tier2Total; // Tier 2 eligible balance
@@ -33,6 +36,7 @@ contract Lynx is Ownable, ERC20 {
     mapping(address wallet => bool dividendExcepmtionStatus)
         public isDividendExempt;
     mapping(address lpAddress => bool) public isLpAddress;
+    mapping(address executor => bool isExecutor) public isSnapshotter;
 
     uint private constant MAX_SUPPLY = 5_000_000 ether;
     uint private constant TIER_1 = 50_000 ether; // TIER 1 is top TIER
@@ -40,6 +44,7 @@ contract Lynx is Ownable, ERC20 {
     uint private constant TAX_PERCENT = 100;
     IUniswapV2Router02 public router;
 
+    address public blacklister;
     address public mainPair;
     address private immutable WETH;
     address payable public immutable ADMIN_WALLET;
@@ -51,6 +56,7 @@ contract Lynx is Ownable, ERC20 {
     uint public sellTax = 5;
 
     bool private isSwapping = false;
+    bool public tradingEnabled = false;
 
     //---------------------------------------------------------------------------------
     // Events
@@ -62,15 +68,36 @@ contract Lynx is Ownable, ERC20 {
     event BlacklistWalletsUpdate(address[] _users, bool blacklisted);
     event SetAddressAsLp(address indexed _lpAddress, bool isLpAddress);
     event SnapshotTaken(uint indexed snapId, uint timestamp);
+    event TradingEnabled(bool isEnabled);
+    event UpdateBlacklister(address indexed _blacklister);
+    event SetSnapshotterStatus(address indexed _snapshotter, bool status);
+    event EditMaxWalletAmount(uint newAmount);
+    event EditTax(uint newTax, bool buyTax, bool sellTax);
+
+    //---------------------------------------------------------------------------------
+    // Modifiers
+    //---------------------------------------------------------------------------------
+    modifier adminOrBlacklister() {
+        if (msg.sender != owner() && msg.sender != blacklister)
+            revert LYNX__NotAllowed();
+        _;
+    }
+
+    modifier onlySnapshotter() {
+        if (!isSnapshotter[msg.sender]) revert LYNX__NotAllowed();
+        _;
+    }
 
     //---------------------------------------------------------------------------------
     // Constructor
     //---------------------------------------------------------------------------------
-    constructor(address _admin) ERC20("LYNX", "LYNX") {
-        _mint(msg.sender, MAX_SUPPLY);
+    constructor(address _admin, address _newOwner) ERC20("LYNX", "LYNX") {
+        _transferOwnership(_newOwner);
+        blacklister = 0x89a022f3983Fa81Ae5B02b5A7d471AB1AC0BcC64;
+        _mint(_newOwner, MAX_SUPPLY);
 
         maxWallet = (MAX_SUPPLY * 1_5) / 100_0; // 1.5% of total supply
-        taxThreshold = MAX_SUPPLY / 100_0; // 0.1% of total supply
+        taxThreshold = MAX_SUPPLY / 100_00; // 0.01% of total supply
 
         // Ethereum Mainnet UniswapV2 Router
         router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
@@ -85,14 +112,20 @@ contract Lynx is Ownable, ERC20 {
         isExcludedFromMaxWallet[address(this)] = true;
         isExcludedFromMaxWallet[owner()] = true;
         isExcludedFromMaxWallet[address(router)] = true;
+        isExcludedFromMaxWallet[address(mainPair)] = true;
 
+        isExcludedFromTax[owner()] = true;
         isExcludedFromTax[address(this)] = true;
         isExcludedFromTax[address(router)] = true;
 
+        isDividendExempt[owner()] = true;
         isDividendExempt[address(this)] = true;
         isDividendExempt[address(router)] = true;
         isDividendExempt[address(mainPair)] = true;
+
+        isSnapshotter[owner()] = true;
         ADMIN_WALLET = payable(_admin);
+        _approve(address(this), address(router), type(uint).max);
     }
 
     //---------------------------------------------------------------------------------
@@ -129,9 +162,10 @@ contract Lynx is Ownable, ERC20 {
      * @notice Set the address as Blacklisted
      * @param _user Address which status will be updated
      */
-    function blacklistAddress(address _user) external onlyOwner {
+    function blacklistAddress(address _user) external adminOrBlacklister {
         isBlacklisted[_user] = true;
         isDividendExempt[_user] = true;
+        _updateSnapDecrease(_user, balanceOf(_user));
         emit BlacklistWalletUpdate(_user, true);
     }
 
@@ -139,10 +173,13 @@ contract Lynx is Ownable, ERC20 {
      * @notice Set the addresses as Blacklisted
      * @param _users Addresses which status will be updated
      */
-    function blacklistAddresses(address[] calldata _users) external onlyOwner {
+    function blacklistAddresses(
+        address[] calldata _users
+    ) external adminOrBlacklister {
         for (uint i = 0; i < _users.length; i++) {
             isBlacklisted[_users[i]] = true;
             isDividendExempt[_users[i]] = true;
+            _updateSnapDecrease(_users[i], balanceOf(_users[i]));
         }
         emit BlacklistWalletsUpdate(_users, true);
     }
@@ -151,9 +188,10 @@ contract Lynx is Ownable, ERC20 {
      * @notice Remove the address as Blacklisted
      * @param _user Addresses which status will be updated
      */
-    function unblacklistAddress(address _user) external onlyOwner {
+    function unblacklistAddress(address _user) external adminOrBlacklister {
         isBlacklisted[_user] = false;
         isDividendExempt[_user] = false;
+        _updateSnapIncrease(_user, balanceOf(_user));
         emit BlacklistWalletUpdate(_user, false);
     }
 
@@ -163,10 +201,11 @@ contract Lynx is Ownable, ERC20 {
      */
     function unblacklistAddresses(
         address[] calldata _users
-    ) external onlyOwner {
+    ) external adminOrBlacklister {
         for (uint i = 0; i < _users.length; i++) {
             isBlacklisted[_users[i]] = false;
             isDividendExempt[_users[i]] = false;
+            _updateSnapIncrease(_users[i], balanceOf(_users[i]));
         }
         emit BlacklistWalletsUpdate(_users, false);
     }
@@ -188,7 +227,7 @@ contract Lynx is Ownable, ERC20 {
     /**
      * @notice Create a snapshot of the current balances
      */
-    function takeSnapshot() external onlyOwner {
+    function takeSnapshot() external onlySnapshotter {
         uint currentSnap = currentSnapId;
         currentSnapId++;
 
@@ -213,6 +252,72 @@ contract Lynx is Ownable, ERC20 {
         taxThreshold = _taxThreshold;
     }
 
+    function setMaxWallet(uint _maxWallet) external onlyOwner {
+        if (_maxWallet < MAX_SUPPLY / 100_00) revert LYNX__InvalidMaxWallet();
+        maxWallet = _maxWallet;
+        emit EditMaxWalletAmount(_maxWallet);
+    }
+
+    /**
+     * @notice set trading as enabled
+     */
+    function enableTrading() external onlyOwner {
+        tradingEnabled = true;
+        emit TradingEnabled(true);
+    }
+
+    /**
+     * @notice set trading as disabled
+     */
+    function pauseTrading() external onlyOwner {
+        tradingEnabled = false;
+        emit TradingEnabled(false);
+    }
+
+    /**
+     * @notice Update the blacklister address
+     * @param _blacklister New blacklister address
+     */
+    function setBlacklister(address _blacklister) external onlyOwner {
+        blacklister = _blacklister;
+        emit UpdateBlacklister(_blacklister);
+    }
+
+    /**
+     * @notice Set the Snapshotter status to an address. These addresses can take snapshots at any time
+     * @param _snapshotter Address to set snapshotter status
+     * @param _isSnapshotter True to set as snapshotter, false to remove
+     */
+    function setSnapshotterAddress(
+        address _snapshotter,
+        bool _isSnapshotter
+    ) external onlyOwner {
+        isSnapshotter[_snapshotter] = _isSnapshotter;
+        emit SetSnapshotterStatus(_snapshotter, _isSnapshotter);
+    }
+
+    /**
+     * @notice set the Buy tax to a new value
+     * @param _buyTax New buy tax
+     * @dev buyTax is a maximimum of 10% so the max acceptable _buyTax is 10
+     */
+    function setBuyTax(uint _buyTax) external onlyOwner {
+        if (_buyTax > 10) revert LYNX__InvalidTaxAmount();
+        buyTax = _buyTax;
+        emit EditTax(_buyTax, true, false);
+    }
+
+    /**
+     * @notice set the Sell tax to a new value
+     * @param _sellTax New sell tax
+     * @dev sellTax is a maximimum of 10% so the max acceptable _sellTax is 10
+     */
+    function setSellTax(uint _sellTax) external onlyOwner {
+        if (_sellTax > 10) revert LYNX__InvalidTaxAmount();
+        sellTax = _sellTax;
+        emit EditTax(_sellTax, false, true);
+    }
+
     //---------------------------------------------------------------------------------
     // Internal & Private Functions
     //---------------------------------------------------------------------------------
@@ -231,11 +336,22 @@ contract Lynx is Ownable, ERC20 {
         if (isBlacklisted[from] || isBlacklisted[to])
             revert LYNX__Blacklisted();
 
-        uint currentBalance = balanceOf(address(this));
-
         bool taxExclusion = isExcludedFromTax[from] || isExcludedFromTax[to];
 
-        if (!isSwapping && currentBalance >= taxThreshold && !taxExclusion) {
+        if (!tradingEnabled && !taxExclusion) {
+            revert LYNX__TradingNotEnabled();
+        }
+
+        _updateSnapDecrease(from, amount);
+
+        uint currentBalance = balanceOf(address(this));
+
+        if (
+            !isSwapping &&
+            currentBalance >= taxThreshold &&
+            !taxExclusion &&
+            !isLpAddress[from] // Cant do this on buys
+        ) {
             _swapTokens();
         }
 
@@ -272,7 +388,6 @@ contract Lynx is Ownable, ERC20 {
         isSwapping = true;
         // Get the current amount of tokens stored in the contract
         uint256 contractTokenBalance = balanceOf(address(this));
-
         // If the contract has tokens
         if (contractTokenBalance > 0) {
             address[] memory path = new address[](2);
@@ -302,7 +417,6 @@ contract Lynx is Ownable, ERC20 {
         uint newBalance = currentBalance - amount;
         SnapshotInfo storage snap = snapshots[currentSnap];
         lastSnapshotId[user] = currentSnap;
-
         // If user is exempt from dividends, we need to set the snapshot value to 0
         if (isDividendExempt[user]) {
             snapshotInfo[user][currentSnap] = 0;
@@ -374,19 +488,19 @@ contract Lynx is Ownable, ERC20 {
             }
             // FROM TIER 2
             else if (currentBalance >= TIER_2 && currentBalance < TIER_1) {
-                snap.tier2Total -= currentBalance;
                 // TO TIER 1
                 if (newBalance >= TIER_1)
                     snap.tier1Total += newBalance;
 
                     // TO SAME TIER
                 else if (newBalance >= TIER_2) snap.tier2Total += newBalance;
+                snap.tier2Total -= currentBalance;
             }
             // FROM TIER 1
             else if (currentBalance >= TIER_1) {
                 // Stay in same tier
-                snap.tier1Total -= currentBalance;
                 snap.tier1Total += newBalance;
+                snap.tier1Total -= currentBalance;
             }
         }
     }
--- test/LynxToken.t.sol
+++ test/LynxToken.t.sol
@@ -6,17 +6,29 @@ import "../src/LynxToken.sol";
 
 contract TestLynxToken is Test {
     Lynx token;
+    IUniswapV2Router02 router;
+    IUniswapV2Pair mainPair;
 
     address admin = makeAddr("admin");
     address user1 = makeAddr("user1");
     address user2 = makeAddr("user2");
     address black = makeAddr("black");
+    address blacklister = makeAddr("blacklister");
     uint maxSupply = 5_000_000 ether;
     uint tier1 = 50_000 ether;
     uint tier2 = 1_000 ether;
 
     function setUp() public {
-        token = new Lynx(admin);
+        token = new Lynx(admin, address(this));
+        router = token.router();
+        mainPair = IUniswapV2Pair(token.mainPair());
+
+        vm.deal(admin, 10 ether);
+        vm.deal(user1, 10 ether);
+        vm.deal(user2, 10 ether);
+        vm.deal(black, 10 ether);
+
+        token.setBlacklister(blacklister);
     }
 
     function test_init() public {
@@ -26,6 +38,7 @@ contract TestLynxToken is Test {
         assertEq(token.totalSupply(), maxSupply);
         assertEq(token.balanceOf(address(this)), maxSupply);
         assertEq(token.maxWallet(), 75_000 ether);
+        assertEq(token.taxThreshold(), 500 ether);
     }
 
     function test_transfer_regular() public {
@@ -38,16 +51,216 @@ contract TestLynxToken is Test {
         assertEq(time, 0);
         token.transfer(user2, tier1);
 
-        vm.expectRevert();
+        vm.expectRevert(LYNX__TradingNotEnabled.selector);
+        vm.prank(user2);
+        token.transfer(user1, tier1);
+
+        token.enableTrading();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                LYNX__MaxWalletReached.selector,
+                user1,
+                tier1 * 2
+            )
+        );
         vm.prank(user2);
         token.transfer(user1, tier1);
     }
 
     function test_blacklist() public {
+        token.transfer(black, tier1);
+
         token.blacklistAddress(black);
         assertTrue(token.isBlacklisted(black));
 
+        (uint totalTier1, , ) = token.snapshots(0);
+        assertEq(totalTier1, 0);
+
         vm.expectRevert();
         token.transfer(black, tier1);
+
+        vm.prank(blacklister);
+        token.unblacklistAddress(black);
+        assertFalse(token.isBlacklisted(black));
+
+        (totalTier1, , ) = token.snapshots(0);
+        assertEq(totalTier1, tier1);
+    }
+
+    function test_addLiquidity() public {
+        token.approve(address(router), maxSupply);
+        //OWNER CAN ADD LIQUIDITY
+        router.addLiquidityETH{value: 10 ether}(
+            address(token),
+            2_000_000 ether,
+            2_000_000 ether,
+            10 ether,
+            address(this),
+            block.timestamp
+        );
+
+        assertEq(token.balanceOf(address(mainPair)), 2_000_000 ether);
+        assertGt(mainPair.totalSupply(), 0);
+        assertGt(mainPair.balanceOf(address(this)), 0);
+    }
+
+    modifier addedLiquidity() {
+        token.approve(address(router), maxSupply);
+        //OWNER CAN ADD LIQUIDITY
+        router.addLiquidityETH{value: 10 ether}(
+            address(token),
+            2_000_000 ether,
+            2_000_000 ether,
+            10 ether,
+            address(this),
+            block.timestamp
+        );
+        _;
+    }
+
+    function test_disabledTrading() public addedLiquidity {
+        address[] memory path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = address(token);
+
+        // We do not set the exact message since uniswap uses it's own error message...
+        vm.expectRevert();
+        vm.prank(user1);
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{
+            value: 0.1 ether
+        }(0, path, user1, block.timestamp);
+    }
+
+    function test_enabledTrading() public addedLiquidity {
+        token.enableTrading();
+        address[] memory path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = address(token);
+
+        vm.prank(user1);
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{
+            value: 0.1 ether
+        }(0, path, user1, block.timestamp);
+
+        assertGt(token.balanceOf(user1), 0);
+    }
+
+    function test_buyTaxes() public addedLiquidity {
+        token.enableTrading();
+
+        address[] memory path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = address(token);
+
+        vm.prank(user1);
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{
+            value: 0.1 ether
+        }(0, path, user1, block.timestamp);
+
+        uint tokenBalance = token.balanceOf(address(token));
+        uint userBalance = token.balanceOf(user1);
+        assertGt(token.balanceOf(address(token)), 0);
+
+        assertEq(tokenBalance, ((tokenBalance + userBalance) * 5) / 100);
+    }
+
+    function test_sellTaxes() public addedLiquidity {
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = router.WETH();
+
+        token.enableTrading();
+
+        token.transfer(user1, 50_000 ether);
+
+        vm.startPrank(user1);
+        token.approve(address(router), 1_000 ether);
+        uint prevETHBalance = user1.balance;
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            1_000 ether,
+            0,
+            path,
+            user1,
+            block.timestamp
+        );
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(token)), 50 ether);
+        assertGt(user1.balance, prevETHBalance);
+    }
+
+    function test_internalSwap() public addedLiquidity {
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = router.WETH();
+
+        token.enableTrading();
+
+        token.transfer(user1, 10_000 ether);
+        token.transfer(user2, 10_000 ether);
+
+        vm.startPrank(user1);
+        token.approve(address(router), 10_000 ether);
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            10_000 ether,
+            0,
+            path,
+            user1,
+            block.timestamp
+        );
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(token)), 500 ether);
+
+        uint adminBalance = admin.balance;
+
+        vm.prank(user2);
+        token.transfer(user1, 10 ether);
+
+        // Swap was successful
+        assertGt(admin.balance, adminBalance);
+        assertLt(token.balanceOf(address(token)), 1 ether);
+    }
+
+    function test_snapshotTaking() public {
+        token.enableTrading();
+        token.transfer(user1, 50_000 ether);
+        // USER1 is part of tier1
+        (uint totalTier1, uint totalTier2, uint time) = token.snapshots(0);
+        assertEq(totalTier1, 50_000 ether);
+        assertEq(totalTier2, 0);
+        assertEq(time, 0);
+        // USER1 drops to tier2
+        vm.prank(user1);
+        token.transfer(address(this), 1_000 ether);
+
+        (totalTier1, totalTier2, time) = token.snapshots(0);
+        assertEq(totalTier1, 0);
+        assertEq(totalTier2, 49_000 ether);
+        assertEq(time, 0);
+        // Take snapshot
+        token.takeSnapshot();
+
+        (, , time) = token.snapshots(0);
+        assertEq(time, block.timestamp);
+        // Check that new current snapshot has previous totals
+        (totalTier1, totalTier2, time) = token.snapshots(1);
+        assertEq(totalTier1, 0);
+        assertEq(totalTier2, 49_000 ether);
+        assertEq(time, 0);
+        // USER 1 drops to NO TIER
+        vm.prank(user1);
+        token.transfer(address(this), 48_001 ether);
+        (totalTier1, totalTier2, time) = token.snapshots(1);
+
+        assertEq(totalTier1, 0);
+        assertEq(totalTier2, 0);
+        assertEq(time, 0);
+
+        uint snapBalance = token.getUserSnapshotAt(user1, 0);
+        assertEq(snapBalance, 49_000 ether);
+        snapBalance = token.getUserSnapshotAt(user1, 1);
+        assertEq(snapBalance, 999 ether);
     }
 }
